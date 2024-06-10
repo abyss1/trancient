56603fba36beb27e421da79cb8a86f6797907d53
Jelle B <abyss1@users.noreply.github.com>
2024-06-10 15:43:27 +0200

    bit cleaner

--- shell/installer.sh
+++ shell/installer.sh
@@ -7,8 +7,6 @@ case "$(uname)" in
     "Linux") OS="linux" ;;
 esac
 
-echo "Running on $OS"
-
 # Determine home directory and create folders
 HOME_DIR=$HOME
 BIN_DIR=$HOME_DIR/bin
@@ -16,13 +14,21 @@ ETC_DIR=$HOME_DIR/etc
 ZSH_DIR=$HOME_DIR/.zsh
 CONFIG_DIR=$HOME_DIR/.config
 
-# Create directories if they do not exist
-[ ! -d $BIN_DIR ] && mkdir -p $BIN_DIR
-[ ! -d $ETC_DIR ] && mkdir -p $ETC_DIR
-[ ! -d $ZSH_DIR ] && mkdir -p $ZSH_DIR
-[ ! -d $CONFIG_DIR ] && mkdir -p $CONFIG_DIR
+# Function to create directory if it does not exist
+create_dir() {
+    [ ! -d $1 ] && mkdir -p $1 && echo "Created directory $1"
+}
 
-echo "Created directories $BIN_DIR, $ETC_DIR, and $ZSH_DIR"
+# Function to create file if it does not exist
+create_file() {
+    [ ! -f $1 ] && touch $1 && echo "Created file $1"
+}
+
+# Create directories
+create_dir $BIN_DIR
+create_dir $ETC_DIR
+create_dir $ZSH_DIR
+create_dir $CONFIG_DIR
 
 # Install tools & fonts
 if [ "$OS" = "mac" ] || [ "$OS" = "linux" ]; then
@@ -31,8 +37,7 @@ if [ "$OS" = "mac" ] || [ "$OS" = "linux" ]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
         echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.profile
         eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-        mkdir -p $HOME/.zsh
-        echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' > $HOME/.zsh/brew.zsh
+        echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' > $ZSH_DIR/brew.zsh
         source $HOME/.zshrc
     fi
     echo "Installing tools with Homebrew, this will take a while !"
@@ -41,18 +46,13 @@ if [ "$OS" = "mac" ] || [ "$OS" = "linux" ]; then
 #    brew search '/font-.*-nerd-font/' | awk '{ print $1 }' | xargs -I{} brew install {} || true
 fi
 
-echo "Installed tools"
-
 # Create starship.zsh, functions.zsh, and aliases.zsh in .zsh directory
-[ ! -f $ZSH_DIR/starship.zsh ] && echo 'eval "$(starship init zsh)"' > $ZSH_DIR/starship.zsh
-[ ! -f $ZSH_DIR/functions.zsh ] && touch $ZSH_DIR/functions.zsh
-[ ! -f $ZSH_DIR/aliases.zsh ] && touch $ZSH_DIR/aliases.zsh
-
-echo "Created starship.zsh, functions.zsh, and aliases.zsh in $ZSH_DIR"
+create_file $ZSH_DIR/starship.zsh && echo 'eval "$(starship init zsh)"' > $ZSH_DIR/starship.zsh
+create_file $ZSH_DIR/functions.zsh
+create_file $ZSH_DIR/aliases.zsh
 
 # Download starship.toml from GitHub and place it in .config directory
-[ ! -f $CONFIG_DIR/starship.toml ] && curl -o $CONFIG_DIR/starship.toml https://raw.githubusercontent.com/abyss1/trancient/main/dotfiles/starship/starship.toml
-
-echo "Downloaded starship.toml to $CONFIG_DIR"
+STARSHIP_CONFIG=$CONFIG_DIR/starship.toml
+create_file $STARSHIP_CONFIG && curl -k -sS -o $STARSHIP_CONFIG https://raw.githubusercontent.com/abyss1/trancient/main/dotfiles/starship/starship.toml
 
 source $HOME/.zshrc # this will do regardless of outcome of previous step
